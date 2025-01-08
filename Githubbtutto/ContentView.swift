37286cdc9c92f03254858eb34e1742368ba5e654
Alex-techw <abrornabiyev695@gmail.com>
2025-01-08 20:13:13 +0300

    Initial Commit

---
+++ Githubbtutto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Githubbtutto
+//
+//  Created by Abror on 08.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Githubbtutto/GithubbtuttoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubbtuttoApp.swift
+//  Githubbtutto
+//
+//  Created by Abror on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubbtuttoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
